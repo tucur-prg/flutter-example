aa1148125e307cdfd71c4cf4c1257b3d928d4c91
n-shirasaki <n-shirasaki@cladminnoMacBook-Pro-2.local>
2023-10-24 14:16:36 +0900

    srcの作成

--- training/test/training_method_channel_test.dart
+++ training/test/training_method_channel_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:training/training_method_channel.dart';
+import 'package:training/src/training_method_channel.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
