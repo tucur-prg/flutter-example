7e9f61866a9771a1257ad64db1e9e8f150ef9359
n-shirasaki <n-shirasaki@cladminnoMacBook-Pro-2.local>
2023-11-02 13:55:24 +0900

    fixed doc comment

--- training/lib/src/repository/registration.dart
+++ training/lib/src/repository/registration.dart
@@ -1,34 +1,63 @@
+import 'dart:io';
 import 'dart:convert';
 
 import 'package:http/http.dart';
 
+import '../value/access_token.dart';
+
 class Registration {
   final Client _client;
 
-  Registration(Client client) : this._client = client;
+  Registration(Client client) : _client = client;
 
   factory Registration.app() {
     return Registration(Client());
   }
 
-  Future<Map<String, dynamic>> accounts(String name, int age) async {
+  Future<String> _safeApiCall(Function() callback) async {
+    try {
+      final response = await callback() as Response;
+      return _parseResponse(response.statusCode, response.body);
+    } on SocketException {
+      throw Exception('No Internet Connection');
+    }
+  }
+
+  String _parseResponse(int httpStatus, String responseBody) {
+    switch (httpStatus) {
+      case 200:
+      case 201:
+      case 204:
+        return responseBody;
+      case 400:
+      case 401:
+      case 402:
+      case 403:
+      case 404:
+        throw RegistrationException('Http status 400-', httpStatus);
+      case 500:
+      case 501:
+      case 502:
+      case 503:
+        throw RegistrationException('Http status 500-', httpStatus);
+      default:
+        throw Exception('Http status $httpStatus unknown error.');
+    }
+  }
+
+  Future<Map<String, dynamic>> accounts(AccessToken token, String name, int age) async {
     var uri = Uri.parse('http://localhost:8181/api/v1/accounts');
-    var response = await _client.post(uri,
+    var body = await _safeApiCall(() async => await _client.post(uri,
         headers: {
           "Content-Type": "application/json",
+          "Authorization": "bearer ${token.value}",
         },
         body: jsonEncode({
           "name": name,
           "age": age,
-        }));
+        })));
 
-    var body = jsonDecode(response.body);
-
-    if (response.statusCode != 201) {
-      throw RegistrationException(body['error'], -1);
-    }
-
-    return body;
+    return jsonDecode(body);
   }
 }
 
